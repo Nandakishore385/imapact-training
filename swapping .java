b2e0d488b09ebc3a0c60ce32e7bc2bd316de1818
ALETI.NANDAKISHORE <aletinandkishore@gmail.com>
2025-05-08 09:32:00 +0530

    Add files via upload

---
+++ swapping .java
@@ -0,0 +1,13 @@
+// swapping
+public class swapping {
+    public static void main(String[] args) {
+      int a = 10;
+      int b =20;
+      int temp = a;
+      a = b;
+      b = temp;
+      
+      System.out.println(" " + a);
+      System.out.println(" " + b);
+    }
+} 
