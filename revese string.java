baf992269bf4c24408e58c8278f3af180aa1c44f
ALETI.NANDAKISHORE <aletinandkishore@gmail.com>
2025-05-09 10:05:27 +0530

    Add files via upload

---
+++ revese string.java
@@ -0,0 +1,11 @@
+// revese of a string
+public class Main {
+  public static void main(String[] args) {
+    String name = "parul univervity";
+    String another = " ";
+    for(int i=name.length()-1;i>=0;i--){
+    System.out.print(name.charAt(i));
+    }
+    System.out.println(another);
+  }
+}
