a0a0b118036d29a78386e98a459554db25a9c7c2
ALETI.NANDAKISHORE <aletinandkishore@gmail.com>
2025-05-13 09:31:21 +0530

    Add files via upload

---
+++ WrapperClass.java
@@ -0,0 +1,19 @@
+WrapperClass
+public class Main {
+    public static void main(String[] args) {
+      // primitve datatypes
+      // byte, short, int, long, float,double, boolean, charAt
+      int a = 12;
+      Integer b = 12;
+      
+      String name = b.toString();
+      
+      Byte b1 = 4;
+      Short sh = 45;
+      Long l = 4564666666636566L;
+      Float f = 45.5f;
+      Double d = 45.5;
+      Boolean bl = true;
+      
+    }
+}
